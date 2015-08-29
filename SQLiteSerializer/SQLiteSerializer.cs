000455ea40785c2ec19b8e0e1dddd9172908ee19
Rory Brown <gozargozarian@gmail.com>
2015-08-29 15:21:35 -0600

    Initial checkin

---
+++ SQLiteSerializer/SQLiteSerializer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SQLiteSerializer {
+	public class SerializedObjectColumn {
+		public string columnName { get; set; }
+		public string columnType { get; set; }
+		public object columnValue { get; set; }
+		public SerializedObjectColumn(string columnName, string columnType, object columnValue) {
+			this.columnName = columnName;
+			this.columnType = columnType;
+			this.columnValue = columnValue;
+		}
+	}
+
+	public class SerializedObjectTable {
+		string tablename;
+		List<SerializedObjectColumn> columns;
+
+		public SerializedObjectTable(string tablename) {
+			this.tablename = tablename;
+			columns = new List<SerializedObjectColumn>();
+		}
+
+		public void AddColumn(SerializedObjectColumn colDef) {
+			columns.Add(colDef);
+        }
+	}
+
+	public class SQLiteSerializer {
+		protected SQLiteConnection globalConn;
+		protected StringBuilder sqlDefinitionRegion = new StringBuilder();		// sql here defines tables
+        protected StringBuilder sqlDataRegion = new StringBuilder();            // sql here fills tables with data
+
+		protected int primaryKeyCount = 0;
+		protected List<SerializedObjectTable> activeTables = new List<SerializedObjectTable>();
+
+		public void Serialize(object target, string databaseConnectionString) {
+			buildInfoTables();
+			buildComplexObjectTables(target);		// if this is just a simple ValueType object, then I shall slap you with a mackrel
+
+			// write it all out
+			openSQLConnection(databaseConnectionString);
+			if (isSQLiteReady()) {
+				SQLiteCommand cmd = new SQLiteCommand(sqlDefinitionRegion.Append(sqlDataRegion).ToString(), globalConn);
+				cmd.ExecuteNonQuery();
+			}
+			closeSQLConnection();
+		}
+
+		public T Deserialize<T>(string databaseConnectionString, bool ignoreMissing = false) {
+			T container = default(T);
+
+			return container;
+		}
+
+		#region Internal Serialization Functions
+		protected void buildInfoTables() {
+		}
+
+		protected int buildComplexObjectTables(object target) {
+			Type localType = target.GetType();
+			if (!canSerialize(target))
+				throw new Exception("Your object is not serializable! Please add [Serializable] to the class definition for " + localType.Name + " and all child objects you are attempting to store.");
+
+			//localType.Module	- TODO: add this to info tables
+			//localType.AssemblyQualifiedName	- TODO: add this to info tables
+			SerializedObjectTable table = new SerializedObjectTable(localType.FullName);
+
+			FieldInfo[] fields = localType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+			foreach (FieldInfo field in fields) {
+				Type fieldType = field.FieldType;
+                if (fieldType.IsPrimitive || fieldType.IsEnum || fieldType.IsValueType || fieldType.Equals(typeof(string)) || fieldType.IsSubclassOf(typeof(ValueType))) {
+					// we can store this raw
+					SerializedObjectColumn col = new SerializedObjectColumn(field.Name, fieldType.FullName, field.GetValue(target));
+                    table.AddColumn(col);
+				} else {
+					// this is a complex type (a class or something), so recurse
+					int FK = buildComplexObjectTables(field.GetValue(target));	// we need a Foreign Key (otherwise objects would randomize themselves across the object structure on each load [goofy effect])
+
+                    SerializedObjectColumn col = new SerializedObjectColumn(field.Name, fieldType.FullName, FK);
+					table.AddColumn(col);
+				}
+			}
+
+			activeTables.Add(table);
+			return ++primaryKeyCount;
+		}
+
+		public bool canSerialize(object target) {
+			Type targetType = target.GetType();
+			return Attribute.IsDefined(targetType, typeof(SerializableAttribute)) || targetType.IsSerializable || (target is ISerializable);
+        }
+		#endregion
+
+		#region SQLite specific functions
+		public SQLiteConnection getRawConnection() { return globalConn; }
+
+		protected bool isSQLiteReady() {
+			return globalConn.State == ConnectionState.Open || globalConn.State == ConnectionState.Executing || globalConn.State == ConnectionState.Fetching;
+		}
+
+		// databaseConnectionString = can actually just be a file path, but optional pass complex connection strings
+		protected void openSQLConnection(string databaseConnectionString) {
+			if (databaseConnectionString.IndexOf("URI") < 0)
+				databaseConnectionString = "URI=file:" + databaseConnectionString;
+            globalConn = new SQLiteConnection(databaseConnectionString);
+			globalConn.Open();
+		}
+
+		protected void closeSQLConnection() {
+			globalConn.Close();
+		}
+		#endregion
+	}
+}
---
+++ SQLiteSerializerTests/ReflectionTest.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace SQLiteSerializerTests {
+	/// <summary>
+	/// Summary description for ReflectionTest
+	/// </summary>
+	[TestClass]
+	public class ReflectionTest {
+		protected class Garbage {
+			public string pubField;
+			public string pubProp { get; set; }
+
+			protected string proField;
+			protected string proProp { get; set; }
+
+			protected string PropNoSetter { get; }
+
+			public Garbage() {
+				pubField = "pubField";
+				pubProp = "pubProp";
+				proField = "proField";
+				proProp = "proProp";
+				PropNoSetter = "PropNoSetter";
+			}
+
+			public string ConfirmTestPropNoSetter() { return PropNoSetter; }
+			public string ConfirmTestProProp() { return proProp; }
+		}
+
+		public ReflectionTest() {}
+
+		#region Context Junk
+		private TestContext testContextInstance;
+		public TestContext TestContext {
+			get {
+				return testContextInstance;
+			}
+			set {
+				testContextInstance = value;
+			}
+		}
+		#endregion
+
+		#region Additional test attributes
+		//
+		// You can use the following additional attributes as you write your tests:
+		//
+		// Use ClassInitialize to run code before running the first test in the class
+		// [ClassInitialize()]
+		// public static void MyClassInitialize(TestContext testContext) { }
+		//
+		// Use ClassCleanup to run code after all tests in a class have run
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+		//
+		// Use TestInitialize to run code before running each test 
+		// [TestInitialize()]
+		// public void MyTestInitialize() { }
+		//
+		// Use TestCleanup to run code after each test has run
+		// [TestCleanup()]
+		// public void MyTestCleanup() { }
+		//
+		#endregion
+
+		[TestMethod]
+		public void TestReflectionProperties() {
+			Garbage test = new Garbage();
+
+			Type tType = test.GetType();
+			PropertyInfo[] props = tType.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+			Assert.IsTrue(props.Length == 3);
+        }
+
+		[TestMethod]
+		public void TestReflectionFields() {
+			Garbage test = new Garbage();
+
+			Type tType = test.GetType();
+			FieldInfo[] fields = tType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+			Assert.IsTrue(fields.Length == 5);
+		}
+
+		[TestMethod]
+		public void TestReflectionSetPrivates() {
+			Garbage test = new Garbage();
+
+			Type tType = test.GetType();
+			PropertyInfo[] props = tType.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance);
+			PropertyInfo prop = props[0];
+			if (prop.CanWrite)
+				prop.SetValue(test, "Something New");
+			prop = props[1];
+			if (prop.CanWrite)
+				prop.SetValue(test, "Something New");		// this throws an exception because it can't write
+
+			Assert.IsTrue(test.ConfirmTestProProp() == "Something New");
+			Assert.IsTrue(test.ConfirmTestPropNoSetter() != "Something New");
+		}
+
+		[TestMethod]
+		public void TestReflectionSetAllFields() {
+			Garbage test = new Garbage();
+
+			Type tType = test.GetType();
+			FieldInfo[] fields = tType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+			foreach (var field in fields) {
+				field.SetValue(test, "Something New");
+			}
+
+			Assert.IsTrue(test.pubField == "Something New");
+			Assert.IsTrue(test.pubProp == "Something New");
+			Assert.IsTrue(test.ConfirmTestPropNoSetter() == "Something New");
+			Assert.IsTrue(test.ConfirmTestProProp() == "Something New");
+		}
+	}
+}
